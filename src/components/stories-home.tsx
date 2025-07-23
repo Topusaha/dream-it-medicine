cfc2b291778162f9af3917cac169e283483e6d1f
Topu Saha <topusaha@Topus-Air.lan>
2025-07-22 20:20:36 -0400

    changed pics

--- src/components/stories-home.tsx
+++ src/components/stories-home.tsx
@@ -15,7 +15,7 @@ const stories: Story[] = [
       title: "Emergency Medicine Physician",
       description:
         "When I entered college, I was a resident assistant, and would occasionally take sick and inebriated students to the emergency room. Now looking back, it’s almost as if the universe was gently pushing me to practice emergency medicine. To me, medicine is more than just taking care of illnesses and diagnosing pathologies, but more so about the art in medicine, and the humanistic approach to treating ailments.",
-      imageUrl: "../assets/dr.Sayegh-pic.jpg",
+      imageUrl: "..src/assets/dr.Sayegh-pic.jpg",
     },
     {
       id: 2,
@@ -23,7 +23,7 @@ const stories: Story[] = [
       title: "Emergency Medicine Physician",
       description:
         "You have to make a real effort to stay psychologically and physically healthy, and to use your time as wisely as possible. Try to enjoy the experience. It’s a challenging four years, but every now and then, take a step back and remember why you started this journey.",
-      imageUrl: "../assets/dr.molina-pic.png",
+      imageUrl: "..src/assets/dr.molina-pic.png",
     },
   ];
   
